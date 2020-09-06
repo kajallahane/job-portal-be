3de9becee8d3aefa415be41d9c5823b19b10587e
kajallahane <kajallahane1993@gmail.com>
2020-09-06 10:52:20 +0530

    Add jobrequests get and post request, with read and write to json file

---
+++ src/main/java/com/jobportal/controller/JobRequestsController.java
@@ -0,0 +1,61 @@
+package com.jobportal.controller;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jobportal.domain.JobRequest;
+
+@RestController
+@CrossOrigin
+public class JobRequestsController {
+	@GetMapping("/jobrequests")
+    public List<JobRequest> listjobrequests() {
+    	List<JobRequest> jobRequests = null;
+    	try {
+    		ObjectMapper mapper = new ObjectMapper();
+			TypeReference<List<JobRequest>> typereference = new TypeReference<List<JobRequest>>() {};
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/jobrequests.json");
+			jobRequests = mapper.readValue(inputStream, typereference);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return jobRequests;
+    	
+        // return "App is working fine";
+    }
+	
+	@PostMapping("/jobrequests")
+    public List<JobRequest> intsertjobrequest(@RequestBody JobRequest newjob) {
+    	List<JobRequest> jobRequests = null;
+    	try {
+    		ObjectMapper mapper = new ObjectMapper();
+			TypeReference<List<JobRequest>> typereference = new TypeReference<List<JobRequest>>() {};
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/jobrequests.json");
+			jobRequests = mapper.readValue(inputStream, typereference);
+			jobRequests.add(newjob);
+			// mapper.updateValue(TypeReference.class.getResourceAsStream("/json/jobrequests.json"), jobRequests);
+			File file = new ClassPathResource("json/jobrequests.json").getFile();
+			mapper.writeValue(file, jobRequests);
+
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return jobRequests;
+    	
+        // return "App is working fine";
+    }
+}
---
+++ src/main/java/com/jobportal/domain/JobRequest.java
@@ -0,0 +1,36 @@
+package com.jobportal.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
+public class JobRequest {
+	
+	String jobRequestId;
+	String jobRequestStatus;
+	Job jobDetails;
+	Candidate candidateDetails;
+	public String getJobRequestId() {
+		return jobRequestId;
+	}
+	public void setJobRequestId(String jobRequestId) {
+		this.jobRequestId = jobRequestId;
+	}
+	public String getJobRequestStatus() {
+		return jobRequestStatus;
+	}
+	public void setJobRequestStatus(String jobRequestStatus) {
+		this.jobRequestStatus = jobRequestStatus;
+	}
+	public Job getJobDetails() {
+		return jobDetails;
+	}
+	public void setJobDetails(Job jobDetails) {
+		this.jobDetails = jobDetails;
+	}
+	public Candidate getCandidateDetails() {
+		return candidateDetails;
+	}
+	public void setCandidateDetails(Candidate candidateDetails) {
+		this.candidateDetails = candidateDetails;
+	}
+}
