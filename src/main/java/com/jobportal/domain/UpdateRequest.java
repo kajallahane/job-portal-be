413f9b197d51053d689580f3663493541eba303c
kajallahane <kajallahane1993@gmail.com>
2020-09-08 20:12:46 +0530

    added

---
+++ src/main/java/com/jobportal/domain/UpdateRequest.java
@@ -0,0 +1,27 @@
+package com.jobportal.domain;
+
+public class UpdateRequest {
+	private String jobId;
+	private String newStatus;
+	private String recruiterId;
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public String getNewStatus() {
+		return newStatus;
+	}
+	public void setNewStatus(String newStatus) {
+		this.newStatus = newStatus;
+	}
+	public String getRecruiterId() {
+		return recruiterId;
+	}
+	public void setRecruiterId(String recruiterId) {
+		this.recruiterId = recruiterId;
+	}
+	
+	
+}
